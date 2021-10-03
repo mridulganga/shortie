cdf2a032920d6610d02956be83a986670b120be3
varunbheemaiah <32514558+varunbheemaiah@users.noreply.github.com>
2021-10-03 23:13:42 +0530

    New UI (#7)
    * Getting URL List on Page Load
    
    * Fixed app name
    
    * Removed Unnecessary Files
    
    * Added gitignore
    
    * Revamped UI
    
    * Added route to fetch domain
    
    Co-authored-by: varun-bheemaiah <varunbheemaiah9@deshik.in>

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,50 +1,168 @@
 <script>
-	let code = '';
-	let url = '';
+	let code = "";
+	let url = "";
 	$: listofurls = [];
-	
-	async function addURL () {
-		const res = await fetch('/', {
-			method: 'POST',
+	let taglines = [
+		"Zuckerberg is Sus",
+		"12345 is a bad password!",
+		"90% bug free!",
+		"Any computer is a laptop if you're brave enough!",
+		"Child's play!",
+		"Cogito ergo sum!",
+		"Supercalifragilisticexpialidocious!",
+	];
+	$: tagline = taglines[Math.floor(Math.random() * taglines.length)];
+	$: href = "";
+
+	async function addURL() {
+		const res = await fetch("/", {
+			method: "POST",
 			body: JSON.stringify({
-				"code": code,
-				"url": url
+				code: code,
+				url: url,
 			}),
 			headers: {
-				"content-type":"application/json"
-			}
-		})
+				"content-type": "application/json",
+			},
+		});
 	}
-	
-	async function getUrls(){
-			const res = await fetch("/?list=true");
-			let urls = await res.json();
-			console.log(urls)
-			listofurls = urls
-	}
-	getUrls()
 
-	setInterval(getUrls, 2000)
+	async function getUrls() {
+		const res = await fetch("/?list=true");
+		let urls = await res.json();
+		listofurls = urls;
+	}
+
+	async function getDomain() {
+		const res = await fetch("/domain");
+		let domain = await res.text();
+		console.log(domain);
+		href = domain;
+	}
+	getUrls();
+	getDomain();
+
+	setInterval(getUrls, 2000);
 </script>
 
-<h2>Shorty</h2>
+<!-- URL LIST -->
+<!-- <div class="container has-text-centered pt-5">
+	<h3 class="title is-4">List of Generated URLs</h3>
+</div> -->
+<!-- <div class="table-container is-fluid pt-5 has-text-centered">
+	{#if listofurls.length > 0}
+		<table class="table is-striped is-fullwidth is-responsive">
+			<thead>
+				<th>Shortie URL</th>
+				<th>Full URL</th>
+			</thead>
+			<tbody>
+				{#each listofurls as item}
+					<tr>
+						<td><a href="{href+item.code}">{href}{item.code}</a></td>
+						<td><a href="{item.url}">{item.url}</a></td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{:else}
+		<h3 class="title is-5">No URLs to display</h3>
+	{/if} -->
+<!-- </div> -->
+<!-- <div class="container"> -->
+	<div class="columns">
+		<div class="column is-3" />
+		<div class="column">
+			<!-- PAGE TITLE -->
+			<section class="hero is-link">
+				<div class="hero-body">
+					<div class="container has-text-centered">
+						<p class="title">Shortie - The URL Shortener</p>
+						<p class="subtitle">
+							{tagline}
+						</p>
+					</div>
+				</div>
+			</section>
 
-<input type="text" placeholder="code" bind:value={code}>
-<input type="text" placeholder="url" bind:value={url}>
-<button on:click={addURL}>Add URL</button>
+			<!-- GENERATION FORM -->
+			<!-- <div class="container is-fluid has-text-centered pt-5"> -->
+				<div class="box">
+					<!-- <h3 class='title is-4'>Generate your own Shortie URL</h3> -->
+					<div class="container pt-5">
+						<div class="field is-horizontal">
+							<div class="field-label is-normal">
+								<label class="label" style="text-align: left;"
+									>{href}</label
+								>
+							</div>
+							<div class="field-body">
+								<div class="field">
+									<p class="control is-expanded">
+										<input
+											class="input"
+											type="text"
+											placeholder="Short URL"
+											bind:value={code}
+										/>
+									</p>
+								</div>
+								<div class="field">
+									<p class="control is-expanded">
+										<input
+											class="input"
+											type="text"
+											placeholder="Full URL"
+											bind:value={url}
+										/>
+									</p>
+								</div>
+							</div>
+							<div class="field pl-2">
+								<div class="control">
+									<button
+										class="button is-link"
+										on:click={addURL}
+									>
+										Shortie!
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			<!-- </div> -->
+			{#each listofurls as item}
+				<div class="box">
+					<div class="card-content">
+						<p class="is-size-4">
+							<a href={"https://"+href + item.code}>{href}{item.code}</a>
+						</p>
+						<p class="is-size-6">
+							{item.url}
+						</p>
+					</div>
+				</div>
+			{/each}
+		</div>
+		<div class="column is-3" />
+	</div>
+<!-- </div> -->
 
-<br><br>
-<table>
-	<thead>
-		<th>Code</th>
-		<th>URL</th>
-	</thead>
-	<tbody>
-		{#each listofurls as item}
-			<tr>
-				<td>{item.code}</td>
-				<td>{item.url}</td>
-			</tr>
-		{/each}
-	</tbody>
-</table>
+<!-- GENERATION FORM -->
+<!-- <div class="container is-fluid has-text-centered pt-5 is-link">
+	<div class="notification is-link">
+		<div class="columns">
+			<div class="column is-2"><h3 class='title'>Shorty</h3></div>
+			<div class="column is-1">{href}</div>
+			<div class="column is-2">
+				<input
+					class="input"
+					type="text"
+					placeholder="Short URL"
+					bind:value={code}
+				/>
+			</div>
+		</div>
+	</div>
+</div> -->
